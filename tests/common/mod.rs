b0e93b649695f144e8de17bc0362535fc189cd69
martinohmann <martinohmann@gmail.com>
2025-05-11 13:22:32 +0200

    test: more integration tests for `TimeSpec`

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -10,3 +10,12 @@ pub fn series_take<B: RangeBounds<DateTime>, R: Repeat>(
     let series = Series::new(range, repeat);
     series.iter().take(take).collect()
 }
+
+pub fn series_take_rev<B: RangeBounds<DateTime>, R: Repeat>(
+    range: B,
+    repeat: R,
+    take: usize,
+) -> Vec<Event> {
+    let series = Series::new(range, repeat);
+    series.iter().rev().take(take).collect()
+}
